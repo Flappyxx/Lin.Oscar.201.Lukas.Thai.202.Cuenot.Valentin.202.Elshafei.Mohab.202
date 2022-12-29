ec5211c11094f07284b7dcdb20f63a6a48a2eac6
Lukas THAI <105665425+Lukas-Thai@users.noreply.github.com>
2022-12-29 16:04:46 +0100

    Add files via upload

---
+++ pages/getEndroit.php
@@ -0,0 +1,12 @@
+
+<?php
+session_start();
+require_once('connectSQL.php');
+$sql="SELECT Latitude, Longitude, Nom, Description, Path FROM  endroit";
+$commande = $pdo->prepare($sql);
+$bool = $commande->execute();
+if ($bool) {
+        $resultat = $commande->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($resultat);
+}
+?>
