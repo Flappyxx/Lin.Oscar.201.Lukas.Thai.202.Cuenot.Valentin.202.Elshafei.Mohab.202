4abf9f3b1ac9e242c2275f3e8d4301caf27a09b4
Lukas THAI <105665425+Lukas-Thai@users.noreply.github.com>
2022-12-29 16:04:15 +0100

    Add files via upload

--- js/carte.js
+++ js/carte.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 
 	var map = L.map('map', {center: [37.40507, 139.72412],zoom: 6});
 	var base = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-	var liste_marqueur=[];
+	var liste_studio=[];
+	var liste_endroit=[];
+	var icone_endroit = L.icon({
+    iconUrl: '../img/endroit.svg',
+	iconSize:     [40, 40],
+	iconAnchor:   [20, 10]
+	});
 	$.ajax({
         	//L'URL de la requête 
         	url: "getStudio.php",
@@ -19,10 +25,34 @@ $(document).ready(function(){
 		for(let ligne=0;ligne<response.length;ligne++){
 			let studio = L.marker([parseFloat(response[ligne].Latitude), parseFloat(response[ligne].Longitude)]);
 			studio.addTo(map);
-			liste_marqueur.push(studio);
+			liste_studio.push(studio);
 			let html ="<h3>Studio "+response[ligne].Nom+"</h3><p>"+response[ligne].Description+"</p><center><img width = '100%' src='"+response[ligne].Path+"'></center>";
 			let popup = L.popup({keepInView:true,closeButton:true}).setContent(html);
 			studio.bindPopup(popup);
+		}
+    	})
+		
+		$.ajax({
+        	//L'URL de la requête 
+        	url: "getEndroit.php",
+
+	        //La méthode d'envoi (type de requête)
+	        method: "GET",
+		dataType:"json"
+		
+    	})
+    	//Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
+    	/*On peut par exemple convertir cette réponse en chaine JSON et insérer
+    	 * cette chaine dans un div id="res"*/
+    	.done(function(response){
+		for(let ligne=0;ligne<response.length;ligne++){
+			let endroit = L.marker([parseFloat(response[ligne].Latitude), parseFloat(response[ligne].Longitude)],{icon: icone_endroit});
+			endroit.addTo(map);
+			liste_endroit.push(endroit);
+			let html ="<h3>"+response[ligne].Nom+"</h3><p>"+response[ligne].Description+"</p><center><img width = '100%' src='"+response[ligne].Path+"'></center>";
+			let popup = L.popup({keepInView:true,closeButton:true}).setContent(html);
+			
+			endroit.bindPopup(popup);
 		}
     	})
 	});
